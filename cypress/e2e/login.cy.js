4f3f0a8ab8481f834dddea5d6dce59d67fc3c2f9
enzo frias borda <enzo_17_05@hotmail.com>
2023-11-08 19:03:15 -0300

    add constant

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,38 +1,40 @@
+import testData from '../constants/data.json'
 describe('inicio', () => {
 
   it('home page should have texts', () => {
     cy.visit('https://hotel-california.vercel.app/')
     cy.get('.grid').click()
 
-    cy.get('.w-30').contains('Login')
-    cy.get('.w-30').contains('Enter your account')
-    cy.get('.w-30').contains('Log in')
-    cy.get('.w-30').contains("Don't have an account")
-    cy.get('.flex-1').contains('Contactanos')
-    cy.get('.flex-1').contains('Registrate')
-    cy.get('body > footer').contains('Somos estudiantes de la Tecnicatura Superior de Software')
-  })
+    // Accede a los datos del archivo JSON
+    const homePageTexts = testData.homepageTexts;
+
+    cy.get('.w-30').contains(homePageTexts.loginText);
+    cy.get('.w-30').contains(homePageTexts.enterText);
+    cy.get('.w-30').contains(homePageTexts.loginButton);
+    cy.get('.w-30').contains(homePageTexts.noAccountText);
+    cy.get('.flex-1').contains(homePageTexts.contactUsText);
+    cy.get('.flex-1').contains(homePageTexts.registerText);
+    cy.get('body > footer').contains(homePageTexts.footerText);
+  });
 
   it('login should work', () => {
-    cy.get('input.w-full:nth-child(1)').type('Enzo_17_05@hotmail.com')
-    cy.get('input.w-full:nth-child(2)').type('12345678')
-    cy.get('.bg-orange-500').click()
-  })
+    const loginCredentials = testData.loginCredentials;
 
+    cy.get('input.w-full:nth-child(1)').type(loginCredentials.email);
+    cy.get('input.w-full:nth-child(2)').type(loginCredentials.password);
+    cy.get('.bg-orange-500').click();
+  });
 
-  it('exit buttonshould be on navbar', () => {
-    cy.get('.flex-1').contains('Registrate')
-    cy.get('.h-8').should('have.css', 'display').and('eq', 'block')
-  })
+  it('exit button should be on navbar', () => {
+    cy.get('.flex-1').contains(testData.homepageTexts.registerText);
+    cy.get('.h-8').should('have.css', 'display').and('eq', 'block');
+  });
 
   it('logout', () => {
-    cy.get('.h-8').click()
+    cy.get('.h-8').click();
 
-    cy.get('nav.flex > a:nth-child(2)').contains('Registrate')
-    cy.get('.flex-1').contains('Contactanos')
-    cy.get('.text-gray-900').should('have.css', 'display').and('eq', 'block')
-
-  })
-
-
-})
+    cy.get('nav.flex > a:nth-child(2)').contains(testData.homepageTexts.registerText);
+    cy.get('.flex-1').contains(testData.homepageTexts.contactUsText);
+    cy.get('.text-gray-900').should('have.css', 'display').and('eq', 'block');
+  });
+});
